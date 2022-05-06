6aa2b2992ec681a4ae047845145648471f3da6f1
shikure <shikur.yennus@gmail.com>
2022-05-05 20:36:21 -0700

    check in code aws env

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello AWS World!")
+#print("Hello AWS World!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/health")
+async def gethealth():
+    return {"test for the first get API using fastAPI"}
